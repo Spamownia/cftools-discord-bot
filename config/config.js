0e54c50057a80bc679a57305b513d5e79c21ce0c
Spamownia <spamownia91@gmail.com>
2026-03-23 19:25:29 +0100

    Update config.js

--- config/config.js
+++ config/config.js
@@ -3,40 +3,39 @@ const { PermissionsBitField } = require('discord.js');
 const config = {
   // Bot activity
   presence: {
-    // One of online, idle, invisible, dnd
     status: 'online',
     activities: [
       {
         name: '/help',
-        // One of Playing, Streaming, Listening, Watching
         type: 'Listening'
       }
     ]
   },
 
-  // Permission config
+  // Permission config – teraz z env!
   permissions: {
-    // Array of Moderator role ids
-    moderatorRoleIds: [ '0', '0' ],
-    // Array of Administrator role ids
-    administratorRoleIds: [ '76561197992396189' ],
-    // Bot Owner, highest permission level (5)
-    ownerId: '76561197992396189',
-
-    // Bot developers, second to highest permission level (4)
-    developers: [ '76561197992396189' ]
+    moderatorRoleIds: process.env.MODERATOR_ROLE_IDS 
+      ? process.env.MODERATOR_ROLE_IDS.split(',') 
+      : ['0', '0'],
+    
+    administratorRoleIds: process.env.ADMIN_ROLE_IDS 
+      ? process.env.ADMIN_ROLE_IDS.split(',') 
+      : [],
+    
+    ownerId: process.env.BOT_OWNER_ID || '76561197992396189',
+    
+    developers: process.env.BOT_DEVELOPERS 
+      ? process.env.BOT_DEVELOPERS.split(',') 
+      : ['76561197992396189']
   },
 
-  // Additional permissions that are considered required when generating
-  // the bot invite link with /invite
   permissionsBase: [
     PermissionsBitField.Flags.ViewChannel,
     PermissionsBitField.Flags.SendMessages,
     PermissionsBitField.Flags.SendMessagesInThreads
   ],
 
-  // The Discord server invite to your Support server
-  supportServerInviteLink: 'https://discord.mirasaki.dev'
+  supportServerInviteLink: process.env.SUPPORT_INVITE || 'https://discord.mirasaki.dev'
 };
 
 module.exports = config;
